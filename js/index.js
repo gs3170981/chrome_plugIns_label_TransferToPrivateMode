1ccf578d993a837720cd357d5c6aeb04be7cc40f
郭胜 <wb-gs465826@antfin.com>
2018-12-29 18:06:15 +0800

    【chrome】扩展插件：成功 - 1

--- js/index.js
+++ js/index.js
@@ -1,46 +1,84 @@
+class tabsOpen {
+  constructor() {
+    this.queryNow();
+  }
+  queryNow() {
+    chrome.tabs.query({
+        currentWindow: true
+      },
+      res => {
+        this.queryNowData = res;
+        this.createNow();
+      }
+    );
+  }
+  createNow() {
+    chrome.windows.create({
+        url: this.queryNowData[0].url,
+        incognito: true,
+        focused: true
+      },
+      res => {
+        if (!res) {
+          chrome.notifications.create(null, {
+            type: 'basic',
+            iconUrl: 'img/error.png',
+            title: '开启失败!',
+            message: '您需要允许该扩展在隐身模式下启动'
+          });
+          return
+        }
+        this.createNowData = res;
+        this.queryNowTabs();
+      }
+    );
+  }
+  queryNowTabs() {
+    chrome.tabs.query({
+        active: true
+      },
+      res => {
+        this.queryNowTabsData = res;
+        this.createCopy();
+      }
+    );
+  }
+  createCopy() {
+    let tabIncognito = this.queryNowTabsData.map(
+      item => item.incognito === true && item
+    );
+    let tabNow = tabIncognito[tabIncognito.length - 1];
+    this.createCopyArr = [];
+    for (let i = 1; i < this.queryNowData.length; i++) {
+      this.createCopyArr.push(
+        new Promise((resolve, reject) => {
+          chrome.tabs.duplicate(tabNow.id, tabCopy => {
+            resolve(tabCopy);
+          });
+        })
+      );
+    }
+    this.updateTabs();
+  }
+  updateTabs() {
+    Promise.all(this.createCopyArr).then(res => {
+      res.map((item, i) => {
+        chrome.tabs.update(item.id, {
+          url: this.queryNowData[i + 1].url
+        });
+      });
+      chrome.notifications.create(null, {
+        type: 'basic',
+        iconUrl: 'img/success.png',
+        title: '开启成功!',
+        message: `您共开启了${res.length + 1}个tabs页面`
+      });
+    });
+  }
+}
 chrome.contextMenus.create({
-	title: "将当前所有页转至隐私模式打开",
-	onclick: () => {
-		chrome.tabs.query({
-			currentWindow: true,
-		}, res => {
-			// res.map(item => {
-			// 	chrome.windows.create({
-			// 		"url": item.url,
-			// 		"incognito": true
-			// 	})
-			// })
-			chrome.windows.create({
-				"url": res[0].url,
-				"incognito": true
-			}, _res => {
-				chrome.tabs.query({
-					active: true,
-					// url: res[0].url,
-				}, tabArray => {
-					let tabIncognito = tabArray.map(item => item.incognito === true && item)
-					let tabNow = tabIncognito[tabIncognito.length - 1]
-					// chrome.tabs.create({
-					// 	windowId: tabNow.windowId,
-					// 	url: ''
-					// })
-					let arr = []
-					for (let i = 1; i < res.length; i++) {
-						arr.push(new Promise((resolve, reject) => {
-							chrome.tabs.duplicate(tabNow.id, tabCopy => {
-								resolve(tabCopy)
-							})
-						}))
-					}
-					Promise.all(arr).then(results => {
-						results.map((_item, _i) => {
-							chrome.tabs.update(_item.id, {
-								url: res[_i + 1].url
-							})
-						})
-					})
-				})
-			})
-		})
-	}
-})
+  title: "将当前所有页转至隐私模式打开",
+  onclick: () => {
+    new tabsOpen();
+  }
+});

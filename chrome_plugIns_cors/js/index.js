de0dede3e4cfd490f360cf6dbba025545752eb06
郭胜 <wb-gs465826@antfin.com>
2019-01-04 19:56:26 +0800

    【chrome】扩展插件：core研发 - 成功！！！

--- chrome_plugIns_cors/js/index.js
+++ chrome_plugIns_cors/js/index.js
@@ -1,133 +1,111 @@
-const _methodsHandle = {
-  'POST': res => {
-
-  },
-  'GET': res => {
-    let content = {
-      asd: 'asd'
+const WATCH = {
+  is: true,
+  init(arr) {
+    if (!this.is) { // 只执行一遍监听
+      return
     }
-    fetch(
-      'https://xiaoce-timeline-api-ms.juejin.im/v1/getListByLastTime?uid=59fbe6c66fb9a045186a159a&client_id=1546398254217&token=eyJhY2Nlc3NfdG9rZW4iOiJjRWpYVDBVcFZOVEZybnpaIiwicmVmcmVzaF90b2tlbiI6Ilp1WHRtbVRmSTBGU1NzMEEiLCJ0b2tlbl90eXBlIjoibWFjIiwiZXhwaXJlX2luIjoyNTkyMDAwfQ%3D%3D&src=web&pageNum=1'
-    ).then(response => {
-      if (response.status === 200) {
-        return response.json();
-      }
-    }).then(json => {
-      console.log(json);
-    }).catch(err => {
-      console.log(err);
-    });
-
-    return {
-      // redirectUrl: '../json/index.json'
-      // redirectUrl: `data:application/json;charset=UTF-8,content`
-      redirectUrl: 'http://127.0.0.1:5500/chrome_plugIns_cors/index.html'
-    }
+    this.is = false
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+      res => {
+        let refererIndex = res.requestHeaders.findIndex(
+          item => item.name === "Referer"
+        );
+        if (~refererIndex) {
+          res.requestHeaders[refererIndex].value = res.url;
+        } else {
+          res.requestHeaders.push({
+            name: 'Referer',
+            value: res.url
+          });
+        }
+        return {
+          requestHeaders: res.requestHeaders
+        };
+      }, {
+        urls: arr.map(item => (item.val + '*'))
+      },
+      ["blocking", "requestHeaders"]
+    );
   }
 }
-chrome.webRequest.onBeforeSendHeaders.addListener(res => {
-  debugger
-  // res.requestHeaders[0].value = 'https://juejin.im'
-  // res.requestHeaders[3].value = 'https://juejin.im'
-  // debugger
-  // res.statusCode = 200
-  return {
-    requestHeaders: res.requestHeaders
-  }
+const INJECTION = obj => {
+  chrome.tabs.executeScript(obj.tabRes[0].id, {
+    code: `window.postMessage('${JSON.stringify(obj)}', '${
+      obj.tabRes[0].url
+    }')`,
+    allFrames: true,
+    runAt: "document_start"
+  });
+}
 
-}, {
-  urls: ["*://*/v1/*"],
-}, ["blocking", "requestHeaders"]);
-chrome.webRequest.onBeforeRequest.addListener(res => {
-
-
-  // res.requestHeaders[0].value = 'https://juejin.im'
-  // res.requestHeaders[3].value = 'https://juejin.im'
-  debugger
-  // let a
-  // $.ajax({
-  //   type: 'get',
-  //   async: false,
-  //   // url: 'https://web-api.juejin.im/gptzllpbev',
-  //   url: 'https://xiaoce-timeline-api-ms.juejin.im/v1/getListByLastTime?uid=59fbe6c66fb9a045186a159a&client_id=1546398254217&token=eyJhY2Nlc3NfdG9rZW4iOiJjRWpYVDBVcFZOVEZybnpaIiwicmVmcmVzaF90b2tlbiI6Ilp1WHRtbVRmSTBGU1NzMEEiLCJ0b2tlbl90eXBlIjoibWFjIiwiZXhwaXJlX2luIjoyNTkyMDAwfQ%3D%3D&src=web&pageNum=1',
-  //   contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
-  //   // contentType: 'application/json;charset=UTF-8',
-  //   // data: JSON.stringify(data),
-  //   data: {
-  //     asd: 123
-  //   },
-  //   // data: '{"current_page":1,"start_date":"2017-10-01","page_size":10,"end_date":"2017-12-20"}',
-  //   success: (res) => {
-  //     debugger
-  //     console.log(res)
-  //     a = res
-  //   },
-  //   error: res => {
-  //     console.log(res)
-  //   }
-  // })
-  // debugger
-  return {
-    // redirectUrl: 'http://127.0.0.1:5500,' + JSON.stringify(a)
-    // redirectUrl: 'https://xiaoce-timeline-api-ms.juejin.im/v1/getListByLastTime?uid=59fbe6c66fb9a045186a159a&client_id=1546398254217&token=eyJhY2Nlc3NfdG9rZW4iOiJjRWpYVDBVcFZOVEZybnpaIiwicmVmcmVzaF90b2tlbiI6Ilp1WHRtbVRmSTBGU1NzMEEiLCJ0b2tlbl90eXBlIjoibWFjIiwiZXhwaXJlX2luIjoyNTkyMDAwfQ%3D%3D&src=web&pageNum=1'
-  }
-  // return _methodsHandle[res.method](res)
-
-  // fetch(
-  //   'https://xiaoce-timeline-api-ms.juejin.im/v1/getListByLastTime?uid=59fbe6c66fb9a045186a159a&client_id=1546398254217&token=eyJhY2Nlc3NfdG9rZW4iOiJjRWpYVDBVcFZOVEZybnpaIiwicmVmcmVzaF90b2tlbiI6Ilp1WHRtbVRmSTBGU1NzMEEiLCJ0b2tlbl90eXBlIjoibWFjIiwiZXhwaXJlX2luIjoyNTkyMDAwfQ%3D%3D&src=web&pageNum=1'
-  // ).then(response => {
-  //   if (response.status === 200) {
-  //     return response.json();
-  //   }
-  // }).then(json => {
-  //   console.log(json);
-  // }).catch(err => {
-  //   console.log(err);
-  // });
-
-  // return {
-  //   cancel: details.url.indexOf("://www.evil.com/") != -1
-  // }
-  // return {
-  //   requestHeaders: res.requestHeaders
-  // }
-
-}, {
-  urls: ["*://*/v1/*"],
-}, ["blocking", "requestBody"]);
-// chrome.webRequest.onResponseStarted.addListener(res => {
-//   // res.requestHeaders[1].value = 'https://juejin.im'
-//   console.log(res)
-//   debugger
-//   // return {
-//   //   requestHeaders: [{
-//   //       name: 'Accept',
-//   //       value: '*/*'
-//   //     },
-//   //     {
-//   //       name: 'Accept-Encoding',
-//   //       value: 'gzip, deflate, br'
-//   //     }, {
-//   //       name: 'Accept-Language',
-//   //       value: 'zh-CN,zh;q=0.9'
-//   //     }, {
-//   //       name: 'Connection',
-//   //       value: 'keep-alive'
-//   //     }, {
-//   //       name: 'Host',
-//   //       value: 'xiaoce-timeline-api-ms.juejin.im'
-//   //     }, {
-//   //       name: 'Origin',
-//   //       value: 'https://juejin.im'
-//   //     }, {
-//   //       name: 'Referer',
-//   //       value: 'https://juejin.im/timeline'
-//   //     }, {
-//   //       name: 'User-Agent',
-//   //       value: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-//   //     },
-//   //   ]
-//   // };
-// }, {
-//   urls: ["<all_urls>"]
-// }, ["blocking", "responseHeaders"]);
+chrome.webRequest.onBeforeRequest.addListener( // 监听当前ajax
+  netRes => {
+    let contentType;
+    let data;
+    let url = decodeURI(netRes.url);
+    let is_chrome_plugIns_cors = url.includes('_chrome_plugIns_cors')
+    let obj
+    if (!is_chrome_plugIns_cors) { // 不是该监听的则不监听
+      return
+    }
+    _chrome_plugIns_cors = url.replace(/^.+?_chrome_plugIns_cors\=/, "")
+    url = url.replace(`_chrome_plugIns_cors=${_chrome_plugIns_cors}`, '')
+    url = url.substring(0, url.length - 1)
+    _chrome_plugIns_cors = JSON.parse(_chrome_plugIns_cors)
+    obj = _chrome_plugIns_cors.find(item => (url.includes(item.key)))
+    if (!obj) { // 无传参则不监听
+      return
+    }
+    url = url.replace(url.substring(0, url.indexOf(obj.key) + obj.key.length), obj.val)
+    chrome.tabs.query({
+        currentWindow: true,
+        highlighted: true,
+        active: true
+      },
+      tabRes => {
+        WATCH.init(_chrome_plugIns_cors) // 监听下面的ajax
+        if (!tabRes || !tabRes[0]) { // 找不到tab标签则不监听
+          console.error('标签未找到！')
+          return
+        }
+        if (netRes.requestBody.formData) {
+          contentType = "application/x-www-form-urlencoded;charset=UTF-8";
+          data = netRes.requestBody.formData;
+          Object.keys(data).forEach(key => {
+            data[key] = data[key] + "";
+          });
+        } else {
+          contentType = "application/json;charset=UTF-8";
+          // TODO 约定值为_DATA
+          data = url.replace(/^.+?_DATA\=/, "");
+        }
+        $.ajax({
+          type: netRes.method,
+          url: url,
+          contentType: contentType,
+          data: data,
+          success: res => INJECTION({
+            data: res,
+            tabRes: tabRes,
+            KEY: url,
+            is: true
+          }),
+          error: res => INJECTION({
+            data: res,
+            tabRes: tabRes,
+            KEY: url,
+            is: false
+          })
+        });
+      }
+    );
+    // debugger
+    return {
+      // cancel: true
+      redirectUrl: "javascript:"
+    };
+  }, {
+    urls: ["*://127.0.0.1/*", "*://localhost/*"]
+  },
+  ["blocking", "requestBody"]
+);
